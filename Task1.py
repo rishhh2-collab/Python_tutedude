e37194afda3e19ea1198009923ed6d3716dee3ce
rishhh2-collab <rishhh2@gmail.com>
2025-08-20 02:18:04 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+#Project 1
+
+user_input1 = int(input("Enter a Number: "))
+user_input2 = int(input("Enter a Number: "))
+addition = user_input1 + user_input2
+substraction = user_input1 - user_input2
+multiplication = user_input1 * user_input2
+divide = user_input1/user_input2
+
+print("Addition: ",  addition)
+print("Substraction: ",  substraction)
+print("Multiplication: ", multiplication)
+print("Division: ", divide)
+
---
+++ task 2.py
@@ -0,0 +1,3 @@
+user_input1 = input("Enter Your first name: ")
+user_input2 = input("Enter Your Last Name: ")
+print("Hello", user_input1," ", user_input2 , "Its great to Meet you")R
